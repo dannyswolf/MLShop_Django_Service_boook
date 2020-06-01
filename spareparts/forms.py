bd31696094605a70a1e8c110ec926b9b7fffaba6
dannys <ntinisiordanis@gmail.com>
2020-06-01 22:00:52 +0300

    upload 'spareparts.forms.py'

---
+++ spareparts/forms.py
@@ -0,0 +1,25 @@
+
+from .models import SpareParts
+from django import forms
+from machines.models import Machines
+from service_data.models import ServiceData
+from datetime import datetime
+from customers.models import Customer
+
+class AddSparePartsToService(forms.ModelForm):
+    PARTS_NR = forms.CharField(max_length=50)
+    ΠΕΡΙΓΡΑΦΗ = forms.CharField(widget=forms.Textarea, required=False)
+    ΚΩΔΙΚΟΣ = forms.CharField(max_length=10, required=False)
+    ΤΕΜΑΧΙΑ = forms.CharField(max_length=100, required=False)
+    ΠΑΡΑΤΗΡΗΣΗΣ = forms.CharField(widget=forms.Textarea, required=False)
+    Customer_ID = forms.ModelChoiceField(queryset=Customer.objects.all(), required=True)
+    ΜΗΧΑΝΗΜΑ = forms.CharField(max_length=100, required=False)
+    Service_ID = forms.IntegerField()
+    Calendar_ID = forms.IntegerField()
+
+
+    class Meta:
+        model = SpareParts
+        fields = '__all__'
+
+        
